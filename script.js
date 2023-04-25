f228b8f71419da5d8fbef8ddda79bcd6a5bdae98
NicolasColombero88 <129215051+NicolasColombero88@users.noreply.github.com>
2023-04-24 21:44:42 -0300

    main
    Primera subida

---
+++ script.js
@@ -0,0 +1,28 @@
+const candidateId = '2e2f13a4-1ec3-4506-82bd-e1b58f81a31b';
+const urlAPI = 'https://challenge.crossmint.io/api'
+let data;
+
+const getGoal = async (url, id) => {
+    const response = await fetch(url + '/map/' + id + '/goal')
+    const data = await response.json()
+    console.log(data)
+    return data
+}
+
+
+const polyanets = async (url, rowValue, colValue, action) => {
+    const response = await fetch(url, {
+        method: action,
+        body: JSON.stringify({
+            row : rowValue,
+            columb : colValue,
+        }),
+    })
+
+    const data = await response.json()
+
+    return data
+}
+
+getGoal(urlAPI, candidateId);
+polyanets(urlAPI, 1,1, 'POST');
