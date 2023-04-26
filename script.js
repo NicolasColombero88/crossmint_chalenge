9c65713d58e9a0984eae1d95e568436550b43668
NicolasColombero88 <129215051+NicolasColombero88@users.noreply.github.com>
2023-04-26 17:08:48 -0300

    se sube mas JS

--- script.js
+++ script.js
@@ -1,37 +1,168 @@
 const candidateId = '2e2f13a4-1ec3-4506-82bd-e1b58f81a31b';
+
 const urlAPI = 'https://challenge.crossmint.io/api';
-const urlGoal = `https://challenge.crossmint.io/api/map/${candidateId}/goal`;
+const urlGoal = `${urlAPI}/map/${candidateId}/goal`;
+const urlMyMap = `${urlAPI}/map/${candidateId}`;
 
-// const Universo = async() => {
-//     let resultado = await fetch (urlGoal);
+const goalPolyCoords = [];
+const goalComethCoords = [];
+const goalSoloonCoords = [];
 
-//     let data = await resultado.json()
-//     console.log(data)
-// }
+const myPolyCoords = [];
+const myComethCoords = [];
+const mySoloonCoords = [];
 
-// Universo();
+const MegaverScan = async() => {
+    try {
+        const scanRes = await fetch(urlGoal);
+        const scanJson = await scanRes.json();
+        const scanGroup = scanJson.goal;
 
-const polyanet = async(id, url, rowValue, colValue) => {
-    try{
-        const toDo = await fetch(`${url}/polyanets`, {
+        console.log(scanGroup)
+
+        for (let y = 0; y < scanGroup.length; y++) {
+            for (let x = 0; x < scanGroup[y].length; x++){
+                
+                if (scanGroup[y][x] == 'POLYANET') {
+                        goalPolyCoords.push({row: y, column: x});
+                } else if(scanGroup[y][x].endsWith('_COMETH')) {
+                        switch (scanGroup[y][x]){
+                            case 'UP_COMETH':
+                                goalComethCoords.push({row: y, column: x, direction: 'up'});
+                            break;
+                            case 'DOWN_COMETH':
+                                goalComethCoords.push({row: y, column: x, direction: 'down'});
+                            break;
+                            case 'LEFT_COMETH':
+                                goalComethCoords.push({row: y, column: x, direction: 'left'});
+                            break;
+                            case 'RIGHT_COMETH':
+                                goalComethCoords.push({row: y, column: x, direction: 'right'});
+                            break;
+                            default:
+                                console.log('error')
+                        }
+                        
+                } else if (scanGroup[y][x].endsWith('_SOLOON')){
+                        goalSoloonCoords.push({row: y, column: x});
+                }
+
+                if(scanGroup[y][x] == 'POLYANET') {
+                    goalPolyCoords.push({row: y, column: x});
+                }
+
+            };
+        }
+
+       return goalPolyCoords
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const myPolyanets = async() => {
+    try {
+        const res = await fetch(urlMyMap);
+        const polysCountsJson = await res.json();
+        const myPolyanets = polysCountsJson.map.content;
+
+        console.log(myPolyanets)
+
+        for (let y = 0; y < myPolyanets.length; y++) {
+            for (let x = 0; x < myPolyanets[y].length; x++){
+                
+                if(myPolyanets[y][x]) {
+                    myPolyCoords.push({row: y, column: x});
+                }
+            };
+        }
+
+         return myPolyCoords
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const doPolyanets = async () => {
+    for (const { row, column } of goalPolyCoords) {
+        await new Promise(resolve => setTimeout(resolve, 800));
+      await addPolyanet(urlAPI, candidateId, row, column);
+    }
+  }
+
+  const doComeths = async () => {
+    for (const { row, column, direction } of goalComethCoords) {
+        await new Promise(resolve => setTimeout(resolve, 800));
+      await addCometh(urlAPI, candidateId, row, column, direction);
+    }
+  }
+
+  const deletePolyanets = async () => {
+     for (const { row, column } of myPolyCoords) {
+        await new Promise(resolve => setTimeout(resolve, 800));
+          await killPolyanets(urlAPI, candidateId, row, column);
+    }
+  }
+
+const addPolyanet = async(url, id, rowValue, colValue) =>{
+    try {
+        const response = await fetch(`${url}/polyanets`, {
             method: 'POST',
-            body: JSON.stringify({
-                candidateId: id,
-                row : rowValue,
-                column : colValue,
-            }),
             headers: {
-                "Content-type": "application/json; charset=UTF-8",
+                'Content-Type': 'application/json',
             },
-            mode: "no-cors",
-        })
-
-        let data = await toDo.json()
-        console.log(data)
-    } catch (error){
-       console.log(error)
+            body: JSON.stringify({
+                "candidateId": id,
+                "row": rowValue,
+                "column": colValue
+            }),
+        });
+        console.log('Polyanet added:', await response.text());
+    } catch (error) {
+        console.log('Error adding Polyanet:', error);
+    }
 }
-    
+
+const addCometh = async(url, id, rowValue, colValue, direct) =>{
+    try {
+        const response = await fetch(`${url}/comeths`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "candidateId": id,
+                "row": rowValue,
+                "column": colValue,
+                "direction" : direct
+            }),
+        });
+        console.log('Polyanet added:', await response.text());
+    } catch (error) {
+        console.log('Error adding Polyanet:', error);
+    }
 }
 
-polyanet(candidateId, urlAPI, 4, 4)
+
+const killPolyanets = async(url, id, rowValue, colValue) =>{
+    try {
+        const response = await fetch(`${url}/polyanets`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "candidateId": id,
+                "row": rowValue,
+                "column": colValue
+            }),
+        });
+        console.log('Polyanet killed:', await response.text());
+    } catch (error) {
+        console.log('Error killing Polyanet:', error);
+    }
+}
+
+
